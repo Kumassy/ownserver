92c121b4f7b61c5f9ea4f7ff521ab54ea5bc8bdb
Kumassy <kumassyii@gmail.com>
2021-05-27 21:53:07 +0900

    fix test for client handshake

--- src/proxy_server.rs
+++ src/proxy_server.rs
@@ -2,12 +2,7 @@ pub use magic_tunnel::{StreamId, ClientHello};
 use futures::{SinkExt, StreamExt};
 // use log::*;
 use std::net::{SocketAddr, IpAddr};
-use tokio::net::{TcpListener, TcpStream};
-use tokio_tungstenite::{accept_async, client_async, WebSocketStream, tungstenite::{Error, Result}};
-use tokio::io::{AsyncRead, AsyncWrite};
 use tokio::task::JoinHandle;
-use std::marker::Unpin;
-use tokio::sync::oneshot;
 use tracing::{info, Instrument};
 use tracing_subscriber;
 use warp::{Filter, Rejection, ws::{Ws, WebSocket, Message}};
@@ -88,19 +83,9 @@ async fn handle_new_connection(peer: SocketAddr, mut websocket: WebSocket) {
 
 #[tokio::main]
 async fn main() {
-    // pretty_env_logger::init();
     tracing_subscriber::fmt::init();
 
     let addr: SocketAddr = ([0, 0, 0, 0], 5000).into();
-    // let listener = TcpListener::bind(&addr).await.expect("Can't listen");
-    // info!("Listening on: {}", addr);
-
-    // while let Ok((stream, _)) = listener.accept().await {
-    //     let peer = stream.peer_addr().expect("connected streams should have a peer address");
-    //     info!("Peer address: {}", peer);
-
-    //     tokio::spawn(handle_new_connection(peer, stream));
-    // }
     let handle = spawn(addr);
     info!("Listening on: {}", addr);
 
@@ -112,46 +97,34 @@ mod try_client_handshake_test {
     use super::*;
 
     macro_rules! test_helper {
-        // The `tt` (token tree) designator is used for
-        // operators and tokens.
         ($send_value:expr, $expect:expr) => {
-            let (con_tx, con_rx) = oneshot::channel();
-            let (msg_tx, msg_rx) = oneshot::channel();
-            let server = async move {
-                let listener = TcpListener::bind("127.0.0.1:0").await.unwrap();
-                con_tx.send(listener.local_addr().unwrap()).unwrap();
-                let (connection, _) = listener.accept().await.expect("No connections to accept");
-                let stream = accept_async(connection).await;
-                let mut stream = stream.expect("Failed to handshake with connection");
+            let server = warp::ws().map(|ws: Ws| {
+                ws.on_upgrade(|mut websocket| {
+                    async move {
+                        let hello = try_client_handshake(&mut websocket).await;
+                        $expect(hello);
+                        // make sure $expect to be executed
+                        websocket.send(Message::text("ok")).await.expect("send back response");
+                    }
+                })
+            });
 
-                let hello = try_client_handshake(&mut stream).await;
-                msg_tx.send(hello).unwrap();
-            };
-            tokio::spawn(server);
+            let mut client = warp::test::ws()
+                .handshake(server)
+                .await
+                .expect("handshake");
 
-            info!("Waiting for server to be ready");
-            let server_addr = con_rx.await.expect("Server not ready");
-            let server_port = server_addr.port();
-
-            let tcp = TcpStream::connect(server_addr).await.expect("Failed to connect");
-            let url = format!("ws://localhost:{}/", server_port);
-            let (mut stream, _) = client_async(url, tcp).await.expect("Client failed to connect");
-
-            stream.send($send_value).await.expect("failed to send client hello");
-
-            let hello = msg_rx.await.expect("Failed to receive messages");
-
-            $expect(hello);
+            client.send($send_value).await;
+            client.recv().await.expect("recv");
         };
     }
-
 
     #[tokio::test]
     async fn accept_client_hello() {
         let hello = serde_json::to_vec(&ClientHello {
             id: StreamId::generate(),
         }).unwrap_or_default();
-        test_helper!(Message::Binary(hello), |hello: Option<ClientHello>| {
+        test_helper!(Message::binary(hello), |hello: Option<ClientHello>| {
             assert!(hello.is_some());
             let hello = hello.unwrap();
             assert_eq!(hello.id, StreamId("stream_foo".to_string()));
@@ -160,14 +133,14 @@ mod try_client_handshake_test {
 
     #[tokio::test]
     async fn reject_invalid_text_hello() {
-        test_helper!(Message::Text("foobarbaz".into()), |hello: Option<ClientHello>| {
+        test_helper!(Message::text("foobarbaz".to_string()), |hello: Option<ClientHello>| {
             assert!(hello.is_none());
         });
     }
     #[tokio::test]
     async fn reject_invalid_serialized_hello() {
         let hello = serde_json::to_vec(&"malformed".to_string()).unwrap_or_default();
-        test_helper!(Message::Binary(hello), |hello: Option<ClientHello>| {
+        test_helper!(Message::binary(hello), |hello: Option<ClientHello>| {
             assert!(hello.is_none());
         });
     }
