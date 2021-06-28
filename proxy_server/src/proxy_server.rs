379af657b6fcbbef9eb3e5a08da697618205f32a
Kumassy <kumassyii@gmail.com>
2021-06-28 23:38:30 +0900

    add not-tested server main

--- proxy_server/src/proxy_server.rs
+++ proxy_server/src/proxy_server.rs
@@ -3,13 +3,14 @@ use futures::{SinkExt, StreamExt, Stream, Sink, AsyncRead, AsyncWrite, AsyncRead
 // use log::*;
 use std::net::{SocketAddr, IpAddr};
 use tokio::task::JoinHandle;
-use tracing::{info, Instrument};
 use tracing_subscriber;
 use warp::{Filter, Rejection, ws::{Ws, WebSocket, Message}, Error as WarpError};
 use std::convert::Infallible;
 use dashmap::DashMap;
 use lazy_static::lazy_static;
 use std::sync::Arc;
+use tracing::{error, info, Instrument};
+use tokio::net::TcpListener;
 
 mod connected_clients;
 mod active_stream;
@@ -26,11 +27,38 @@ lazy_static! {
 
 #[tokio::main]
 async fn main() {
+    let control_port: u16 = 5000;
+    let remote_port: u16 = 8080;
     tracing_subscriber::fmt::init();
 
-    let addr: SocketAddr = ([0, 0, 0, 0], 5000).into();
-    let handle = spawn(addr);
-    info!("Listening on: {}", addr);
 
-    handle.await.unwrap();
+    tracing::info!("starting server!");
+
+    control_server::spawn(([0, 0, 0, 0], control_port));
+    info!("started tunnelto server on 0.0.0.0:{}", control_port);
+
+    let listen_addr = format!("[::]:{}", remote_port);
+    info!("listening on: {}", &listen_addr);
+
+    // create our accept any server
+    let listener = TcpListener::bind(listen_addr)
+        .await
+        .expect("failed to bind");
+
+    loop {
+        let socket = match listener.accept().await {
+            Ok((socket, _)) => socket,
+            _ => {
+                error!("failed to accept socket");
+                continue;
+            }
+        };
+
+        tokio::spawn(
+            async move {
+                remote::accept_connection(&CONNECTIONS, ACTIVE_STREAMS.clone(), socket, "host-foobar".to_string()).await;
+            }
+            .instrument(tracing::info_span!("remote_connect")),
+        );
+    }
 }
