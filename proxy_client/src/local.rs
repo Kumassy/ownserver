31055fea073165661bd5969f0b5087f426344a01
Kumassy <kumassyii@gmail.com>
2021-07-04 15:22:28 +0900

    refactor test

--- proxy_client/src/local.rs
+++ proxy_client/src/local.rs
@@ -86,49 +86,48 @@ mod tests {
 
     #[tokio::test]
     async fn process_local_tcp_test() {
-        let (con_tx, con_rx) = oneshot::channel();
-        let (msg_tx, msg_rx) = oneshot::channel();
+        let (service_addr_tx, service_addr_rx) = oneshot::channel();
+        let (notify_exit_from_fn_tx, notify_exit_from_fn_rx) = oneshot::channel();
         let (tunnel_tx, mut tunnel_rx) = unbounded();
         let stream_id = StreamId::generate();
         let stream_id_clone = stream_id.clone();
-        let server = async move {
+        let proxy_client_service = async move {
             let listener = TcpListener::bind("127.0.0.1:0").await.unwrap();
-            con_tx.send(listener.local_addr().unwrap()).unwrap();
+            service_addr_tx.send(listener.local_addr().unwrap()).unwrap();
             let (stream, _) = listener.accept().await.expect("No connections to accept");
-            let (reader, writer) = split(stream);
+            let (reader, _writer) = split(stream);
             
 
             let _ = process_local_tcp(reader, tunnel_tx, stream_id_clone).await;
-            msg_tx.send(()).unwrap();
+            notify_exit_from_fn_tx.send(()).unwrap();
         };
-        tokio::spawn(server);
+        tokio::spawn(proxy_client_service);
 
         info!("Waiting for server to be ready");
-        let server_addr = con_rx.await.expect("Server not ready");
-        let server_port = server_addr.port();
+        let service_addr = service_addr_rx.await.expect("Server not ready");
 
-        let mut tcp = TcpStream::connect(server_addr).await.expect("Failed to connect");
-        tcp.write_all(b"some bytes").await.expect("failed to send client hello");
-        tcp.shutdown().await.expect("failed to close stream");
+        let mut local_service = TcpStream::connect(service_addr).await.expect("Failed to connect");
+        local_service.write_all(b"some bytes").await.expect("failed to send client hello");
+        local_service.shutdown().await.expect("failed to close stream");
 
         // process_local_tcp should exit if TcpStream is closed
-        let _ = msg_rx.await.expect("Failed to receive messages");
+        let _ = notify_exit_from_fn_rx.await.expect("Failed to receive messages");
 
 
         // TcpReader data should be present in tunnel_rx
-        assert_eq!(Some(ControlPacket::Data(stream_id.clone(), b"some bytes".to_vec())), tunnel_rx.next().await);
-        assert_eq!(None, tunnel_rx.next().await);
+        assert_eq!(tunnel_rx.next().await, Some(ControlPacket::Data(stream_id.clone(), b"some bytes".to_vec())));
+        assert_eq!(tunnel_rx.next().await, None);
     }
 
     #[tokio::test]
     async fn forward_to_local_tcp_test() {
-        let (con_tx, con_rx) = oneshot::channel();
+        let (service_addr_tx, service_addr_rx) = oneshot::channel();
         let (mut msg_tx, mut msg_rx) = unbounded();
         let (mut tunnel_tx, tunnel_rx) = unbounded();
         let stream_id = StreamId::generate();
-        let server = async move {
+        let proxy_client_service = async move {
             let listener = TcpListener::bind("127.0.0.1:0").await.unwrap();
-            con_tx.send(listener.local_addr().unwrap()).unwrap();
+            service_addr_tx.send(listener.local_addr().unwrap()).unwrap();
             let (stream, _) = listener.accept().await.expect("No connections to accept");
             let (mut reader, _writer) = split(stream);
             
@@ -144,21 +143,21 @@ mod tests {
                 msg_tx.send(data).await.expect("failed to write to msg_tx");
             }
         };
-        tokio::spawn(server);
+        tokio::spawn(proxy_client_service);
 
         tunnel_tx.send(StreamMessage::Data(b"some bytes".to_vec())).await.expect("failed to write to tunnel");
         drop(tunnel_tx);
 
         info!("Waiting for server to be ready");
-        let server_addr = con_rx.await.expect("Server not ready");
+        let service_addr = service_addr_rx.await.expect("Server not ready");
 
-        let stream = TcpStream::connect(server_addr).await.expect("Failed to connect");
+        let stream = TcpStream::connect(service_addr).await.expect("Failed to connect");
         let (_reader, writer) = split(stream);
         // process_local_tcp should exit if TcpStream is closed
         let _ = forward_to_local_tcp(stream_id, writer, tunnel_rx).await;
 
         // Data sent to queue should be wrote to TcpStream
-        assert_eq!(Some(b"some bytes".to_vec()), msg_rx.next().await);
-        assert_eq!(None, msg_rx.next().await);
+        assert_eq!(msg_rx.next().await, Some(b"some bytes".to_vec()));
+        assert_eq!(msg_rx.next().await, None);
     }
 }
