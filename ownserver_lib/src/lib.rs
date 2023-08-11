0d9831ee8f631e63b720caf65cb372395f7c2a32
Kumassy <kumassyii@gmail.com>
2023-08-12 00:45:30 +0900

    add controlpacket v2

--- ownserver_lib/src/lib.rs
+++ ownserver_lib/src/lib.rs
@@ -82,13 +82,23 @@ impl EndpointId {
     }
 }
 
-#[derive(Serialize, Deserialize, Debug, Clone, Copy)]
+#[derive(Serialize, Deserialize, Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub enum Protocol {
     TCP = 6,
     UDP = 17,
 }
 
-#[derive(Serialize, Deserialize, Debug, Clone)]
+impl std::fmt::Display for Protocol {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+         match self {
+            Protocol::TCP => write!(f, "tcp"),
+            Protocol::UDP => write!(f, "udp"),
+        }
+    }
+}
+
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq, Hash)]
 pub struct EndpointClaim {
     pub protocol: Protocol,
     pub local_port: u16,
@@ -127,6 +137,28 @@ impl std::fmt::Display for ControlPacket {
     }
 }
 
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+pub enum ControlPacketV2 {
+    Init(StreamId, EndpointId),
+    Data(StreamId, Vec<u8>),
+    Refused(StreamId),
+    End(StreamId),
+    Ping,
+}
+
+impl std::fmt::Display for ControlPacketV2 {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+         match self {
+            ControlPacketV2::Init(sid, eid) => write!(f, "ControlPacket::Init(sid={}, eid={})", sid, eid),
+            ControlPacketV2::Data(sid, data) => write!(f, "ControlPacket::Data(sid={}, data_len={})", sid,  data.len()),
+            ControlPacketV2::Refused(sid)  => write!(f, "ControlPacket::Refused(sid={})", sid),
+            ControlPacketV2::End(sid) => write!(f, "ControlPacket::End(sid={})", sid),
+            ControlPacketV2::Ping => write!(f, "ControlPacket::Ping"),
+        }
+    }
+}
+
+
 #[derive(Serialize, Deserialize, Debug, Clone)]
 #[serde(rename_all = "snake_case")]
 pub enum ServerHello {
@@ -142,7 +174,7 @@ pub enum ServerHello {
     VersionMismatch,
 }
 
-#[derive(Serialize, Deserialize, Debug, Clone)]
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq, Hash)]
 pub struct Endpoint {
     pub id: EndpointId,
     pub protocol: Protocol,
@@ -157,6 +189,7 @@ pub type Endpoints = Vec<Endpoint>;
 pub enum ServerHelloV2 {
     Success {
         client_id: ClientId,
+        host: String,
         endpoints: Endpoints,
     },
     BadRequest,
@@ -206,6 +239,38 @@ impl Encoder<ControlPacket> for ControlPacketCodec {
 
 impl Decoder for ControlPacketCodec {
     type Item = ControlPacket;
+    type Error = io::Error;
+
+    fn decode(&mut self, src: &mut BytesMut) -> Result<Option<Self::Item>, Self::Error> {
+        if !src.is_empty() {
+            let decoded = rmp_serde::from_slice(src).map_err(|e| io::Error::new(io::ErrorKind::Other, e))?;
+            Ok(Some(decoded))
+        } else {
+            Ok(None)
+        }
+    }
+}
+
+#[derive(Copy, Clone, Debug, Eq, PartialEq, Ord, PartialOrd, Hash, Default)]
+pub struct ControlPacketV2Codec {
+}
+impl ControlPacketV2Codec {
+    pub fn new() -> Self {
+        Self {}
+    }
+}
+impl Encoder<ControlPacketV2> for ControlPacketV2Codec {
+    type Error = io::Error;
+
+    fn encode(&mut self, item: ControlPacketV2, dst: &mut BytesMut) -> Result<(), Self::Error> {
+        let encoded = rmp_serde::to_vec(&item).map_err(|e| io::Error::new(io::ErrorKind::Other, e))?;
+        dst.extend_from_slice(&encoded);
+        Ok(())
+    }
+}
+
+impl Decoder for ControlPacketV2Codec {
+    type Item = ControlPacketV2;
     type Error = io::Error;
 
     fn decode(&mut self, src: &mut BytesMut) -> Result<Option<Self::Item>, Self::Error> {
